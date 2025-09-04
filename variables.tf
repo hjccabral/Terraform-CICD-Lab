a079e1ab1a0a59c5d96b82da9ed5b4d38561c618
hiltoncostacabral <hilton.costa.cabral@gmail.com>
2025-09-04 12:45:56 -0300

    Fix: region s3

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-west-1"
+  default     = "us-east-1"
 }
 
 variable "aws_pub_key" {
